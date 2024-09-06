f7fb6f90ced070f92afc2edc8a4af29a54b13f65
VitorG22 <limavitor447@gmail.com>
2024-09-06 09:18:26 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors:{origin: '*'}})
+
+const PORT = process.env.PORT ?? 3001
+
+io.on('connection', socket => {
+    console.log('User Connect:', socket.id)
+    socket.on('disconnect', reason => console.log('Usuario Desconectado: ', socket.id))
+    
+    socket.on('setUserName', (data) => {
+        socket.data.userName = data.userName
+    })
+
+    socket.on('sendMessage', (data)=>{
+        console.log(data)
+        io.emit('receiveMessage', {
+            authorName: socket.data.userName,
+            message:  data.message,
+            authorId: socket.id
+        })
+    })
+})
+
+server.listen(PORT, ()=> console.log('Server running in PORT: ', PORT))
